90b588981221f5e4384453dc21c5075c7f68277b
Tai Nguyen <nqat2003@gmail.com>
2025-05-06 16:45:00 +0700

    Add main App.vue component

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Game2048 from './components/Game2048.vue'
+</script>
+
+<template>
+  <main>
+    <Game2048 />
+  </main>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
